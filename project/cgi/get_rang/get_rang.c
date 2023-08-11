d7d7d91bae4cc78a757a639ad5de3f4b953e4c28
SUPCAT131 <2338189062@qq.com>
2023-08-11 11:26:37 +0800

    解决 提交表单后打开新页面原内容丢失的问题 主要解决方式在html文件中

--- project/cgi/get_rang/get_rang.c
+++ project/cgi/get_rang/get_rang.c
@@ -16,18 +16,55 @@ char c_illu_low[40]={0};
 
 void showmsg()
 {
+    // v1.0 --效果普通
+    // printf("Content-Type:text/html;charset=utf-8\n\n");
+    // printf("<html>\n");
+    // printf("<head><title>CGI Output</title></head>\n");
+    // printf("<body>\n");
+    // printf("<table border=\"1\">\n");
+    // printf("<tr><th>变量</th><th>原始值</th><th>转换后的值</th></tr>\n");
+    // printf("<tr><td>c_temp_up</td><td>%s</td><td>%d</td></tr>\n", c_temp_up, atoi(c_temp_up));
+    // printf("<tr><td>c_temp_low</td><td>%s</td><td>%d</td></tr>\n", c_temp_low, atoi(c_temp_low));
+    // printf("<tr><td>c_hum_up</td><td>%s</td><td>%d</td></tr>\n", c_hum_up, atoi(c_hum_up));
+    // printf("<tr><td>c_hum_low</td><td>%s</td><td>%d</td></tr>\n", c_hum_low, atoi(c_hum_low));
+    // printf("<tr><td>c_illu_up</td><td>%s</td><td>%d</td></tr>\n", c_illu_up, atoi(c_illu_up));
+    // printf("<tr><td>c_illu_low</td><td>%s</td><td>%d</td></tr>\n", c_illu_low, atoi(c_illu_low));
+    // printf("</table>\n");
+    // printf("</body>\n");
+    // printf("</html>\n");
+
+    // v2.0 --效果不好
+    // printf("Content-type: text/html;charset=utf-8\n\n");
+    // printf("<html>\n");
+    // printf("<head>\n");
+    // printf("<title>CGI Form Result</title>\n");
+    // printf("<script type='text/javascript'>\n");
+    // printf("function showPopup() {\n");
+    // printf("    alert('处理结果：\\n温度上限：%s\\n温度下限：%s\\n湿度上限：%s\\n湿度下限：%s\\n光强上限：%s\\n光强下限：%s');\n", c_temp_up, c_temp_low, c_hum_up, c_hum_low, c_illu_up, c_illu_low);
+    // printf("}\n");
+    // printf("</script>\n");
+    // printf("</head>\n");
+    // printf("<body onload='showPopup()'>\n");
+    // printf("<h1>表单数据已处理</h1>\n");
+    // printf("</body>\n");
+    // printf("</html>\n");
+
+   printf("Content-type: text/html;charset=utf-8\n\n");
     printf("<html>\n");
-    printf("<head><title>CGI Output</title></head>\n");
+    printf("<head>\n");
+    printf("<title>Form Submission Result</title>\n");
+    // ... 其他头部内容 ...
+    printf("<script type='text/javascript'>\n");
+    printf("window.onload = function() {\n");
+    printf("    setTimeout(function() {\n");
+    printf("        window.close();\n");  // 关闭当前窗口
+    printf("    }, 1000);  // 1000 毫秒后关闭页面（1 秒）\n");
+    printf("};\n");
+    printf("</script>\n");
+    printf("</head>\n");
     printf("<body>\n");
-    printf("<table border=\"1\">\n");
-    printf("<tr><th>变量</th><th>原始值</th><th>转换后的值</th></tr>\n");
-    printf("<tr><td>c_temp_up</td><td>%s</td><td>%d</td></tr>\n", c_temp_up, atoi(c_temp_up));
-    printf("<tr><td>c_temp_low</td><td>%s</td><td>%d</td></tr>\n", c_temp_low, atoi(c_temp_low));
-    printf("<tr><td>c_hum_up</td><td>%s</td><td>%d</td></tr>\n", c_hum_up, atoi(c_hum_up));
-    printf("<tr><td>c_hum_low</td><td>%s</td><td>%d</td></tr>\n", c_hum_low, atoi(c_hum_low));
-    printf("<tr><td>c_illu_up</td><td>%s</td><td>%d</td></tr>\n", c_illu_up, atoi(c_illu_up));
-    printf("<tr><td>c_illu_low</td><td>%s</td><td>%d</td></tr>\n", c_illu_low, atoi(c_illu_low));
-    printf("</table>\n");
+    printf("<h1>修改已成功提交</h1>\n");
+    // ... 其他页面内容 ...
     printf("</body>\n");
     printf("</html>\n");
 }
@@ -63,8 +100,8 @@ int cgiMain(int argc, const char *argv[])
     cgiFormString("hum_low",c_hum_low,sizeof(c_hum_low));
     cgiFormString("illu_up",c_illu_up,sizeof(c_illu_up));
     cgiFormString("illu_low",c_illu_low,sizeof(c_illu_low));
-    printf("Content-Type:text/html;charset=utf-8\n\n"); // 告诉服务器返回的是 HTML 内容
-    // showmsg();
+     // 告诉服务器返回的是 HTML 内容
+    showmsg();
 
     k_msgt k_m1={
         .hum[0]=atoi(c_hum_low),
